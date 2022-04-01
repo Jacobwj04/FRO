b2fd71741919e2083b057b8351f94c683e3fcd78
Jacobwj04 <32894@ma-web.nl>
2022-04-01 21:05:29 +0200

    les 5 af

--- les5/js/script.js
+++ les5/js/script.js
@@ -27,3 +27,36 @@ catchButton.onclick = function () {
         }
     }
 }
+
+const showTitle = document.getElementById("js--show-title")
+const showSummary = document.getElementById("js--show-summary")
+
+let wizard = fetch("https://api.tvmaze.com/search/shows?q=the%20flash")
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        const kaas = data[1]
+        showTitle.innerHTML = kaas.show.name;
+        showSummary.innerHTML = kaas.show.summary;
+    });
+
+const nameText = document.getElementById("js--name");
+const inputField = document.getElementById("js--input");
+
+inputField.onkeyup = function (event) {
+    if (event.keyCode === 13) {
+        let name = inputField.value;
+        let age = fetch("https://api.agify.io?name=" + name)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (echteData) {
+                inputField.style.display = "none";
+                nameText.innerHTML = echteData.age;
+            });
+    }
+}
+
+
+
